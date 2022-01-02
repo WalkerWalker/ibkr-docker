fe4cd6ba00eb99ccd68c0db9aa4bd7cab64f6daa
Jiamin Zhu <github@jiamin.io>
2022-01-02 08:54:03 +0100

    make twsstart part of build and config.ini not.

--- app/start.sh
+++ app/start.sh
@@ -9,12 +9,11 @@ Xvfb :0 -ac -screen 0 1920x1080x24 &
 export DISPLAY=:0
 
 # Start VNC server, listening at 5900 by default
-x11vnc -ncache 10 -ncache_cr -viewpasswd remote_view_only_pass -passwd some_pass123  -display :0 -forever -shared -bg -noipv6i &
+x11vnc -ncache 10 -ncache_cr -viewpasswd remote_view_only_pass -passwd some_pass123  -display :0 -forever -shared -bg -noipv6 &
 
 # Start TWS and automatically restart if it dies
-/opt/ibc/twsstart.sh -inline
-#while true; do
-#    /opt/ibc/twsstart.sh -inline
-#    sleep 5
-#done
+while true; do
+    /opt/ibc/twsstart.sh -inline
+    sleep 5
+done
 
